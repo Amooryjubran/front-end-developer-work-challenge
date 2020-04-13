2d1804fba72217e2e2579e39f566ccd7fbbfa83c
Omar Gubran <54118716+Amooryjubran@users.noreply.github.com>
2020-04-12 20:38:29 -0400

    Update Explore-top.js

--- src/components/pages/explore_page/Explore-top.js
+++ src/components/pages/explore_page/Explore-top.js
@@ -6,7 +6,7 @@ const ExploreTop = () => (
       <h1 className="explore-text">EXPLORE</h1>
         <div className="explore-btn">
           <button className="exlore-buttons" style={{backgroundColor: 'whitesmoke', color: 'lightgreen'}}>Explore</button>
-          <Link to='/labs'><button className="exlore-buttons">Challenges</button></Link>
+          <Link to='/labs'><button className="exlore-buttons">Labs</button></Link>
           <Link to='/projects'><button className="exlore-buttons">Projects</button></Link>
         </div>
     </div>
